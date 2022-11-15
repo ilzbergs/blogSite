8b05459c84bfb982f40d55a94ff5c31cd01d1ffc
Toms Ilzbergs <ilzbergs@inbox.lv>
2022-11-15 18:39:53 +0200

    add procfile

--- app.js
+++ app.js
@@ -88,6 +88,11 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+let port = process.env.PORT;
+if (port === null || port == "") {
+  port = 3000;
+}
+app.listen(port, function (req, res) {
+  console.log('Server started successfully!');
 });
